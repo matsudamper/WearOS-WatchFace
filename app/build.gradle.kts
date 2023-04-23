c8b609db1f63bfed689b51bae1e9bb1f2a596434
matsudamper <let@matsudamper.net>
2023-04-24 01:22:32 +0900

    version catalogに移動

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,5 +73,5 @@ dependencies {
     androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
-    implementation("androidx.wear.watchface:watchface-complications-rendering:1.1.1")
+    implementation(libs.watchface.complications.rendering)
 }
